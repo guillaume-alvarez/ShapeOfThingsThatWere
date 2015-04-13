ff09aa5c16b8de4d97dafe73ec92935ce635cb08
galvarez <guillaume.alvarez@gmail.com>
2015-04-13 14:19:57 +0200

    Remove unused variables from AI component.

--- core/src/com/galvarez/ttw/model/components/AIControlled.java
+++ core/src/com/galvarez/ttw/model/components/AIControlled.java
@@ -1,21 +1,9 @@
 package com.galvarez.ttw.model.components;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-
 import com.artemis.Component;
-import com.artemis.Entity;
 import com.galvarez.ttw.model.map.MapPosition;
 
 public final class AIControlled extends Component {
-
-  /** All neighboring empires. */
-  public final Set<Entity> neighbors = new HashSet<>();
-
-  public List<MapPosition> estimatedTiles;
-
-  public int estimationTurn;
 
   public int lastMove;
 
