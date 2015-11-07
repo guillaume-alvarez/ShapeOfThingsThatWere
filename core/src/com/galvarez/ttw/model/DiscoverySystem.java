8dc6f116d27a30fcc0312d38bca7b54104eb230e
galvarez <guillaume.alvarez@gmail.com>
2015-11-07 18:12:25 +0100

    Avoid exception when getting an entity that was just created and not yet processed.

--- core/src/com/galvarez/ttw/model/DiscoverySystem.java
+++ core/src/com/galvarez/ttw/model/DiscoverySystem.java
@@ -260,12 +260,10 @@ public final class DiscoverySystem extends EntitySystem implements EventHandler 
     // collect entities we share influence with
     for (MapPosition p : influences.get(empire).influencedTiles)
       for (Entry inf : map.getInfluenceAt(p))
-        if (inf.key != empire.getId() && !neighbors.containsKey(inf.key)) {
-          Entity neighbor = world.getEntity(inf.key);
-          if (neighbor != null)
-            // may not have been inserted yet?
-            neighbors.put(inf.key, neighbor);
-        }
+        if (inf.key != empire.getId() && !neighbors.containsKey(inf.key)
+        // may be pending insertion into world if just revolted
+            && world.getEntityManager().isActive(inf.key))
+          neighbors.put(inf.key, world.getEntity(inf.key));
 
     return neighbors.values();
   }
